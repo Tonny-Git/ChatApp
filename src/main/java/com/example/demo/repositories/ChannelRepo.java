2d848a7e607b60acf6558d2483a30b5e24750bbd
Ammar sahyoun <ammarzgr@hotmail.com>
2020-03-19 21:37:03 +0100

    Create users, messages, channels classes, and test db.

---
+++ src/main/java/com/example/demo/repositories/ChannelRepo.java
@@ -0,0 +1,12 @@
+package com.example.demo.repositories;
+
+import com.example.demo.entities.Channel;
+import com.example.demo.entities.Message;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ChannelRepo  extends CrudRepository<Channel, Integer> {
+    public Channel findById(int id);
+}
+
