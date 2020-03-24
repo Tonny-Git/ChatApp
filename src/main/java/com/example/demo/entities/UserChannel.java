de8b96cd45d8ffafd43b685665b61b3166d8637a
Ammar sahyoun <ammarzgr@hotmail.com>
2020-03-24 08:40:01 +0100

    friendList, channels

---
+++ src/main/java/com/example/demo/entities/UserChannel.java
@@ -0,0 +1,56 @@
+package com.example.demo.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "userxchannel")
+public class UserChannel {
+
+    @EmbeddedId
+    private UserChannelKey id;
+    private boolean admin;
+
+    public UserChannel() {
+
+    }
+
+    public UserChannel(User user, Channel channel, boolean admin) {
+        this.user = user;
+        this.channel = channel;
+        this.admin = admin;
+    }
+
+    @ManyToOne
+    @MapsId("member_id")
+    @JoinColumn(name = "member_id")
+    User user;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Channel getChannel() {
+        return channel;
+    }
+
+    public void setChannel(Channel channel) {
+        this.channel = channel;
+    }
+
+    @ManyToOne
+    @MapsId("channel_id")
+    @JoinColumn(name = "channel_id")
+    Channel channel;
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+}
